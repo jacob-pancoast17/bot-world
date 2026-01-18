d930b3f1779b6ce2720b926d662d124cbbc184f0
Jacob <jacob.pancoast17@gmail.com>
2026-01-18 14:20:41 -0500

    Test physics steps

--- simulate.py
+++ simulate.py
@@ -1,4 +1,16 @@
 import pybullet as p
+import time
 
+# Creates physics object and connects to GUI
 physicsClient = p.connect(p.GUI)
+
+# When enabled, this disables pybullet sidebars
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+# Step simulator physics 1000 times
+for i in range(0, 1000):
+    p.stepSimulation()
+    time.sleep(.1)
+    print(i)
+
 p.disconnect()
