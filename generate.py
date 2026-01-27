dd283c5c71217c395dcdfb6921c97d028aa58c5b
Jacob <jacob.pancoast17@gmail.com>
2026-01-27 14:00:15 -0500

    Procedurally generate 25 towers

--- generate.py
+++ generate.py
@@ -1,10 +1,9 @@
+import math
 import pyrosim.pyrosim as pyrosim
 
 # Tell pyrosim the name of the file info about the world is stored in
 # This link will contain a box
 pyrosim.Start_SDF("boxes.sdf")
-
-iteration = 0
 
 length = 1
 width = 1
@@ -15,10 +14,18 @@ z = 0
 y = .5
 
 
-while iteration < 10:
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5 + iteration], size=[length, width, height])
-    iteration += 1
-# Stores a box with these specifications to . format
+# Generate a tower
+for i in range(0, 5):
+    for j in range(0, 5):
+        iteration = 0
+        while iteration < 10:
+            pyrosim.Send_Cube(name="Box",
+                            pos=[i, j, 0.5 + iteration],
+                                size=[1 * math.pow(0.9, iteration),
+                                        1 * math.pow(0.9, iteration),
+                                        1 * math.pow(0.9, iteration)])
+            iteration += 1
+#Stores a box with these specifications to . format
 #pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
 #pyrosim.Send_Cube(name="Box2", pos=[1, 0, 1.5], size=[length, width, height])
 
