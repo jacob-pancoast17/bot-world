b5150438d883d76cb2020b9e53db85c7ac578074
Jacob <jacob.pancoast17@gmail.com>
2026-01-27 13:44:24 -0500

    Add a second box

--- generate.py
+++ generate.py
@@ -2,17 +2,18 @@ import pyrosim.pyrosim as pyrosim
 
 # Tell pyrosim the name of the file info about the world is stored in
 # This link will contain a box
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0
 z = 0
-y = 1.5
+y = .5
 
 # Stores a box with these specifications to box.sdf
 pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, z, y], size=[length, width, height])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -14,7 +14,7 @@ p.setGravity(0, 0, -9.8, physicsClient)
 planeId = p.loadURDF("plane.urdf")
 
 # Load our link
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 # Step simulator physics 1000 times
 for i in range(0, 1000):
