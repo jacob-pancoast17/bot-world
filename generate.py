1ebff4a73bbabd5025e59f02dab12e1f3547164f
Jacob <jacob.pancoast17@gmail.com>
2026-01-27 13:49:22 -0500

    Procedurally generate a tower

--- generate.py
+++ generate.py
@@ -4,6 +4,8 @@ import pyrosim.pyrosim as pyrosim
 # This link will contain a box
 pyrosim.Start_SDF("boxes.sdf")
 
+iteration = 0
+
 length = 1
 width = 1
 height = 1
@@ -12,8 +14,12 @@ x = 0
 z = 0
 y = .5
 
-# Stores a box with these specifications to box.sdf
-pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x, z, y], size=[length, width, height])
+
+while iteration < 10:
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5 + iteration], size=[length, width, height])
+    iteration += 1
+# Stores a box with these specifications to . format
+#pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
+#pyrosim.Send_Cube(name="Box2", pos=[1, 0, 1.5], size=[length, width, height])
 
 pyrosim.End()
