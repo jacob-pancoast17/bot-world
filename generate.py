2bf329b464fc10000af348cb445a951584e929a7
Jacob <jacob.pancoast17@gmail.com>
2026-02-02 12:05:23 -0500

    Add Create_World() function

--- generate.py
+++ generate.py
@@ -1,20 +1,13 @@
 import math
 import pyrosim.pyrosim as pyrosim
 
-# Tell pyrosim the name of the file info about the world is stored in
-# This link will contain a box
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+    # Tell pyrosim the name of the file info about the world is stored in
+    pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
+    #Stores a box with these specifications to . format
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
 
-x = 0
-z = 0
-y = .5
+    pyrosim.End()
 
-#Stores a box with these specifications to . format
-pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
-#pyrosim.Send_Cube(name="Box2", pos=[1, 0, 1.5], size=[length, width, height])
-
-pyrosim.End()
+Create_World()
