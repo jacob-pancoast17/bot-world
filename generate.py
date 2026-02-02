4d47698d572c29dfd3d99a8c3b7548d0cf787d3b
Jacob <jacob.pancoast17@gmail.com>
2026-02-02 12:25:05 -0500

    Add first joint, between torso and leg

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ def Create_World():
     pyrosim.Start_SDF("world.sdf")
 
     # Stores a box with these specifications to . format
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[3, 3, 0.5], size=[1, 1, 1])
 
     pyrosim.End()
 
@@ -15,7 +15,10 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
 
     # Stores a box with these specifications to . format
-    pyrosim.Send_Cube(name="Torso", pos=[3, 3, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[1, 0, 1.5], size=[1, 1, 1])
+
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position=[0.5, 0, 1])
 
     pyrosim.End()
 
